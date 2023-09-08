95fd6594e841a48ec3e088105b8718c01c6b6740
skunbydev <skunbydev@gmail.com>
2023-09-08 16:54:18 -0300

    Exercise

---
+++ CursoNelioAlves/src/estruturaRepetitiva/For.java
@@ -0,0 +1,48 @@
+package estruturaRepetitiva;
+
+import java.util.Scanner;
+
+public class For {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Fazer um programa que lê um valor inteiro N
+		 * e depois N números inteiros. Ao final, mostra a soma
+		 *  dos numeros lidos.
+		 */
+		
+		Scanner sc = new Scanner (System.in);
+		
+		
+		/*
+		int N = sc.nextInt();
+		int soma = 0;
+		for (int i = 0; i <N;  i = i+1) {
+			int x = sc.nextInt();
+			soma += x;
+		}
+		System.out.println(soma);
+		*/
+		
+		/*
+		int N = sc.nextInt();
+		int soma = 0;
+		
+		for (int i = 0; i < N; i++) {
+			int x = sc.nextInt();
+			soma += x;
+		}
+		System.out.println(soma);
+		*/
+		/*
+		 * contagem regreesiva 
+		 */
+		
+		for (int i = 4; i >= 0; i--) {
+			System.out.println("Valor de i: "+i);
+			
+		}
+		sc.close();
+	}
+
+}
