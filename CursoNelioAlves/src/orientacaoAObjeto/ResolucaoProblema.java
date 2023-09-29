25ca857b4650943a46640123cd842eda8643e76e
skunbydev <skunbydev@gmail.com>
2023-09-28 23:11:29 -0300

    triangle measure

--- CursoNelioAlves/src/orientacaoAObjeto/ResolucaoProblema.java
+++ CursoNelioAlves/src/orientacaoAObjeto/ResolucaoProblema.java
@@ -3,40 +3,41 @@ package orientacaoAObjeto;
 import java.util.Locale;
 import java.util.Scanner;
 
+import entities.Triangle;
+
 public class ResolucaoProblema {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Locale.setDefault(Locale.US);
-		
-		Scanner sc = new Scanner (System.in);
-		double xA, xB, xC, yA, yB, yC;
-		
-		System.out.println("Enter the measures of triangle X:");
-		xA = sc.nextInt();
-		xB = sc.nextInt();
-		xC = sc.nextInt();
-		
-		System.out.println("Enter the measures of triangle Y:");
-		yA = sc.nextInt();
-		yB = sc.nextInt();
-		yC = sc.nextInt();
-		
-		double p = (xA + xB + xC) / 2.0;
-		double areaX = Math.sqrt(p * (p - xA) * (p - xB) * (p- xC));
-		
-		p = (yA + yB + yC) / 2.0;
-		double areaY = Math.sqrt(p * (p - yA) * (p - yB) * (p- yC));
-		
-		System.out.printf("Triangle X area: %.4f%n \n", areaX);
-		System.out.printf("Triangle y area: %.4f%n \n", areaY);
-		
-		if (areaX > areaY) 
-			System.out.println("Larger area X:");
-		else 
-			System.out.println("Larger area Y:");
-		sc.close();;
-				
-	}
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        Locale.setDefault(Locale.US);
+
+        Scanner sc = new Scanner(System.in);
+
+        Triangle x, y;
+        x = new Triangle();
+        y = new Triangle();
+
+        System.out.println("Enter the measures of triangle X:");
+        x.a = sc.nextDouble();
+        x.b = sc.nextDouble();
+        x.c = sc.nextDouble();
+
+        System.out.println("Enter the measures of triangle Y:");
+        y.a = sc.nextDouble();
+        y.b = sc.nextDouble();
+        y.c = sc.nextDouble();
+
+        double areaX = x.area();
+        double areaY = y.area();
+
+        System.out.printf("Triangle X area: %.4f%n \n", areaX);
+        System.out.printf("Triangle Y area: %.4f%n \n", areaY);
+
+        if (areaX > areaY)
+            System.out.println("Larger area X:");
+        else
+            System.out.println("Larger area Y:");
+        sc.close();
+    }
 
 }
