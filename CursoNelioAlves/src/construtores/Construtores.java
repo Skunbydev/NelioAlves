f14edd1f3b48dceb8bd5198e2d22392c77e96429
skunbydev <skunbydev@gmail.com>
2023-10-16 23:41:11 -0300

    New exercise

--- CursoNelioAlves/src/construtores/Construtores.java
+++ CursoNelioAlves/src/construtores/Construtores.java
@@ -1,10 +1,10 @@
-package aplication;
+package construtores;
 
 import java.util.Locale;
 import java.util.Scanner;
 import entities.Product;
 
-public class Program {
+public class Construtores {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
