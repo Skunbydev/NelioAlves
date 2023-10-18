159e1eec5ae403a70215909460ce1e8b66f82909
skunbydev <skunbydev@gmail.com>
2023-10-17 22:42:47 -0300

    New exercise

--- CursoNelioAlves/src/construtores/GetESetters.java
+++ CursoNelioAlves/src/construtores/GetESetters.java
@@ -2,25 +2,25 @@ package construtores;
 
 import java.util.Locale;
 import java.util.Scanner;
+
 import entities.Product;
 
-public class Construtores {
+public class GetESetters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		
-		
+
 		System.out.println("Enter product data: ");
 		System.out.print("Name: ");
 		String name = sc.nextLine();
 		System.out.print("Price: ");
 		double price = sc.nextDouble();
-			
-		
+
 		Product product = new Product(name, price);
+		
+		
 		
 		System.out.println();
 		System.out.println("Product data: " + product);
@@ -37,7 +37,6 @@ public class Construtores {
 		System.out.println();
 		System.out.println("Updated data: " + product);
 		sc.close();
-
 	}
 
 }
